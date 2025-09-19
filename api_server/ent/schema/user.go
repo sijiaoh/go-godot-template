ef55dc702665ad7d99bd69e29f0b5473b19e76cc
sijiaoh <sijiaoh@outlook.com>
2025-09-20 08:11:52 +0900

    Add user table

---
+++ api_server/ent/schema/user.go
@@ -0,0 +1,27 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("name").
+			Optional().
+			Nillable().
+			NotEmpty().
+			MaxLen(10),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
--- api_server/main.go
+++ api_server/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/sijiaoh/go-godot-template/api_server/ent"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	entClient, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer entClient.Close()
+	// Run the auto migration tool.
+	if err := entClient.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
