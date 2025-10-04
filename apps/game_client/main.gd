f84a33f3c9c7a600b83efe67af474d4a96318f01
sijiaoh <sijiaoh@outlook.com>
2025-10-05 00:56:23 +0900

    Open modal when unauthorized

--- apps/game_client/main.gd
+++ apps/game_client/main.gd
@@ -11,4 +11,5 @@ func _ready() -> void:
 
 
 func _on_authentication_unauthorized() -> void:
-		ScreenEvents.replace_screen.emit(ScreenDatabase.TITLE_SCREEN_ENTRY)
+	ModalEvents.open_modal.emit("认证失败，请重新登录")
+	ScreenEvents.replace_screen.emit(ScreenDatabase.TITLE_SCREEN_ENTRY)
--- apps/game_client/player/me.gd
+++ apps/game_client/player/me.gd
@@ -7,8 +7,6 @@ var name: String
 static func fetch() -> Me:
 	var res := await GameServer.me()
 	if res.status_code != 200:
-		ModalEvents.open_modal.emit("获取用户信息失败: err=%s status_code=%s" % [res.err, res.status_code])
-		# TODO: 制作常用通讯失败处理，返回Title
 		return null
 
 	var me := Me.new()
