255272c1a1611d971ba9c1d97c7c60b01be64ed6
sijiaoh <sijiaoh@outlook.com>
2025-10-08 12:25:43 +0900

    Update title screen

--- apps/game_client/title/title_screen.gd
+++ apps/game_client/title/title_screen.gd
@@ -1,6 +1,7 @@
 extends Screen
 
 
+@export var signup_container: Container
 @export var user_name_input: LineEdit
 
 
@@ -8,3 +9,10 @@ func signup():
 	await Authentication.signup(user_name_input.text)
 	if Authentication.is_logged_in:
 		ScreenEvents.replace_screen.emit(ScreenDatabase.GAME_SCREEN_ENTRY)
+
+
+func _on_covered_button_pressed() -> void:
+	if Authentication.is_logged_in:
+		ScreenEvents.replace_screen.emit(ScreenDatabase.GAME_SCREEN_ENTRY)
+	else:
+		signup_container.visible = true
